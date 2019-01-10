27cb80d1a5f73502a2d02f29ebde9e99d833ce09
Sam Eaton <sameaton1989@gmail.com>
2019-01-10 18:10:20 -0500

    adding working frontpage

--- server.js
+++ server.js
@@ -1,82 +1,213 @@
 // Dependencies
 var express = require("express");
-var mongojs = require("mongojs");
+var logger = require("morgan");
+// var mongojs = require("mongojs");
+var mongoose = require("mongoose");
+var exphbs = require("express-handlebars");
 // Require axios and cheerio. This makes the scraping possible
 var axios = require("axios");
 var cheerio = require("cheerio");
 
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
+
 // Initialize Express
 var app = express();
 
-// Database configuration
-var databaseUrl = "scraper";
-var collections = ["guardData"];
+// Set Handlebars.
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+// Configure middleware
 
-// Main route (simple Hello World Message)
-app.get("/", function(req, res) {
-  res.send("Hello world");
-});
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
-// Retrieve data from the db
-app.get("/all", function(req, res) {
-  // Find all results from the guardData collection in the db
-  db.scrapedData.find({}, function(error, found) {
-    // Throw any errors to the console
-    if (error) {
-      console.log(error);
-    }
-    // If there are no errors, send the data to the browser as json
-    else {
-      res.json(found);
-    }
-  });
-});
 
-// Scrape data from one site and place it into the mongodb db
+// If deployed, use the deployed database. Otherwise use the local scraper database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/scraper";
+
+mongoose.connect(MONGODB_URI);
+
+// A GET route for scraping the echoJS website
 app.get("/scrape", function(req, res) {
-  // Make a request via axios for the news section of `ycombinator`
-  axios.get("https://www.theguardian.com/us").then(function(response) {
-    // Load the html body from axios into cheerio
-    var $ = cheerio.load(response.data);
-    // For each element with a "title" class
-    $("h3").each(function(i, element) {
-      // Save the text and href of each link enclosed in the current element
-      var title = $(element).children("a").text();
-      var link = $(element).children("a").attr("href");
-
-      // If this found element had both a title and a link
-      if (title && link) {
-        // Insert the data in the guardData db
-        db.guardData.insert({
-          title: title,
-          link: link
-        },
-        function(err, inserted) {
-          if (err) {
-            // Log the error if one is encountered during the query
+    // First, we grab the body of the html with axios
+    axios.get("https://www.theguardian.com/us").then(function(response) {
+      // Then, we load that into cheerio and save it to $ for a shorthand selector
+      var $ = cheerio.load(response.data);
+  
+      // Now, we grab every h2 within an article tag, and do the following:
+      $("h3").each(function(i, element) {
+        // Save an empty result object
+        var result = {};
+  
+        // Add the text and href of every link, and save them as properties of the result object
+        result.title = $(this)
+          .children("a")
+          .text();
+        result.link = $(this)
+          .children("a")
+          .attr("href");
+  
+        // Create a new Article using the `result` object built from scraping
+        db.Article.create(result)
+          .then(function(dbArticle) {
+            // View the added result in the console
+            console.log(dbArticle);
+          })
+          .catch(function(err) {
+            // If an error occurred, log it
             console.log(err);
-          }
-          else {
-            // Otherwise, log the inserted data
-            console.log(inserted);
-          }
-        });
-      }
+          });
+      });
+  
+      // Send a message to the client
+      res.send("Scrape Complete");
     });
   });
 
-  // Send a "Scrape Complete" message to the browser
-  res.send("Scrape Complete");
-});
+  app.get("/", function(req, res) {
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        console.log(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        console.log(err);
+      });
+  
+      res.render("index");
+    });
+
+  
+  // Route for getting all Articles from the db
+  app.get("/articles", function(req, res) {
+    // Grab every document in the Articles collection
+    db.Article.find({})
+      .then(function(dbArticle) {
+        // If we were able to successfully find Articles, send them back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+  
+  // Route for grabbing a specific Article by id, populate it with it's note
+  app.get("/articles/:id", function(req, res) {
+    // Using the id passed in the id parameter, prepare a query that finds the matching one in our db...
+    db.Article.findOne({ _id: req.params.id })
+      // ..and populate all of the notes associated with it
+      .populate("note")
+      .then(function(dbArticle) {
+        // If we were able to successfully find an Article with the given id, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+  
+  // Route for saving/updating an Article's associated Note
+  app.post("/articles/:id", function(req, res) {
+    // Create a new note and pass the req.body to the entry
+    db.Note.create(req.body)
+      .then(function(dbNote) {
+        // If a Note was created successfully, find one Article with an `_id` equal to `req.params.id`. Update the Article to be associated with the new Note
+        // { new: true } tells the query that we want it to return the updated User -- it returns the original by default
+        // Since our mongoose query returns a promise, we can chain another `.then` which receives the result of the query
+        return db.Article.findOneAndUpdate({ _id: req.params.id }, { note: dbNote._id }, { new: true });
+      })
+      .then(function(dbArticle) {
+        // If we were able to successfully update an Article, send it back to the client
+        res.json(dbArticle);
+      })
+      .catch(function(err) {
+        // If an error occurred, send it to the client
+        res.json(err);
+      });
+  });
+  
+  // Start the server
+  app.listen(PORT, function() {
+    console.log("App running on port " + PORT + "!");
+  });
+  
+// // Hook mongojs configuration to the db variable
+// var db = mongojs(databaseUrl, collections);
+// db.on("error", function(error) {
+//   console.log("Database Error:", error);
+// });
+
+// // Main route (simple Hello World Message)
+// app.get("/", function(req, res) {
+//   res.send("Hello world");
+// });
+
+// // Retrieve data from the db
+// app.get("/all", function(req, res) {
+//   // Find all results from the guardData collection in the db
+//   db.scrapedData.find({}, function(error, found) {
+//     // Throw any errors to the console
+//     if (error) {
+//       console.log(error);
+//     }
+//     // If there are no errors, send the data to the browser as json
+//     else {
+//       res.json(found);
+//     }
+//   });
+// });
+
+// // Scrape data from one site and place it into the mongodb db
+// app.get("/scrape", function(req, res) {
+//   // Make a request via axios for the Guardian's front page
+//   axios.get("https://www.theguardian.com/us").then(function(response) {
+//     // Load the html body from axios into cheerio
+//     var $ = cheerio.load(response.data);
+//     // For each element with an h3 element
+//     $("h3").each(function(i, element) {
+//       // Save the text and href of each link enclosed in the current element
+//       var title = $(element).children("a").text();
+//       var link = $(element).children("a").attr("href");
+
+//       // If this found element had both a title and a link
+//       if (title && link) {
+//         // Insert the data in the guardData db
+//         db.guardData.insert({
+//           title: title,
+//           link: link
+//         },
+//         function(err, inserted) {
+//           if (err) {
+//             // Log the error if one is encountered during the query
+//             console.log(err);
+//           }
+//           else {
+//             // Otherwise, log the inserted data
+//             console.log(inserted);
+//           }
+//         });
+//       }
+//     });
+//   });
+
+//   // Send a "Scrape Complete" message to the browser
+//   res.send("Scrape Complete");
+// });
 
 
-// Listen on port 3000
-app.listen(8080, function() {
-  console.log("App running on port 8080!");
-});
+// // Listen on port 8080
+// app.listen(8080, function() {
+//   console.log("App running on port 8080!");
+// });
