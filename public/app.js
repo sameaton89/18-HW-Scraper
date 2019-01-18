5ca2a815649c97547171cf9cebc78e928f836760
Sam Eaton <sameaton1989@gmail.com>
2019-01-17 19:27:42 -0500

    Delete button now red and floating right

--- public/app.js
+++ public/app.js
@@ -1,7 +1,7 @@
 // Grab the articles as a json
 $.getJSON("/articles", function(data) {
   // For each one
-  for (var i = 0; i < data.length; i++) {
+  for (var i = data.length - 1; i >= 0; i--) {
     // Display the apropos information on the page
     $("#articles").append("<p class='title' data-id='" + data[i]._id + "'>" + data[i].title + "<br />" + data[i].link + "</p>");
   }
@@ -27,14 +27,53 @@ $(document).on("click", "#savenote", function() {
     .then(function(data) {
       // Log the response
       console.log(data);
-      // Empty the notes section
-      $("#notes").empty();
+
+      $("#notes-modal-body").prepend("Comment posted successfully! <button onclick='reload()'>Go Back</button>");
     });
 
   // Also, remove the values entered in the input and textarea for note entry
   $("#titleinput").val("");
   $("#bodyinput").val("");
 });
+
+function reload() {
+  event.preventDefault;
+  $.ajax({
+    method: "GET",
+    url: "/articles/" + thisId
+  }).then(function(data) {
+    console.log(data);
+    // Now open the modal! (Assuming you are using bootstrap.js)
+    $("#notes-modal").modal("show");
+    // If you used 'res.json' then you can use yourData here
+    $("#notes-modal-body").html(data);
+
+    // The title of the article
+    $("#notes-modal-title").html("<h2>" + data.title + "</h2>");
+    // An input to enter a new title
+    $("#notes-modal-body").html("<input id='titleinput' name='title' >");
+    // A textarea to add a new note body
+    $("#notes-modal-body").append("<br><textarea id='bodyinput' name='body'></textarea>");
+    // A button to submit a new note, with the id of the article saved to it
+    $("#notes-modal-body").append("<br><button data-id='" + data._id + "' type='button' class='btn btn-primary' id='savenote'>Save Note</button>");
+
+
+
+    // If there's a note in the article
+    if (data.note) {
+      // Place the title of the note in the title input
+      // $("#notes-modal-body").append("<p><b>" + data.note[0].title + "</p></b>" + data.note[0].body);
+
+      for (var i = 0; i < data.note.length ; i++) {
+        $("#notes-modal-body").prepend("<p data-id=" +
+        data.note[i]._id + "><b>" + data.note[i].title + "</b><br>" + data.note[i].body + " <span class='delete'>X</span></p>")}
+
+    }
+
+  });
+
+  
+}
 
 $(document).on("click", "#scrape", function() {
   event.preventDefault;
@@ -135,44 +174,3 @@ $(document).on("click", ".delete", function() {
     }
   });
 });
-// Loads results onto the page
-// var getResults = function () {
-//   // Empty any results currently on the page
-//   $("#notes-modal, #notes-modal-body").empty();
-//   // Grab all of the current notes
-//   $.getJSON("/articles", function(data) {
-//     // For each note...
-//     for (var i = 0; i < data.length; i++) {
-//       // ...populate #results with a p-tag that includes the note's title and object id
-//       $("#notes-modal-body").prepend("<p class='data-entry' data-id=" + data[i]._id + "><span class='dataTitle' data-id=" +
-//         data[i]._id + ">" + data[i].title + "</span><span class=delete>X</span></p>");
-//     }
-//   });
-// }
-
-// // When the #make-new button is clicked
-// $(document).on("click", "#save-dat", function() {
-//   // AJAX POST call to the submit route on the server
-//   // This will take the data from the form and send it to the server
-//   $.ajax({
-//     type: "POST",
-//     dataType: "json",
-//     url: "/submit",
-//     data: {
-//       title: $("title").val(),
-//       note: $("note").val(),
-//       created: Date.now()
-//     }
-//   })
-//   // If that API call succeeds, add the title and a delete button for the note to the page
-//     .then(function(data) {
-//     // Add the title and delete button to the #results section
-//       $("#notes-modal-body").prepend("<p class='data-entry' data-id=" + data._id + "><span class='dataTitle' data-id=" +
-//       data._id + ">" + data.title + "</span><span class=delete>X</span></p>");
-//       // Clear the note and title inputs on the page
-//       $("#note").val("");
-//       $("#title").val("");
-//     });
-// });
-
-
